b475d4b8ef4339338b49d55743cb3c5d56b47a43
uiuxpedia <74965805+uiuxpedia@users.noreply.github.com>
2020-11-25 19:10:42 +0700

    Add files via upload

---
+++ membuat kalkulator sederhana.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+//variable
+int angka1;
+int angka2;
+
+//main program
+int main(int argc,char*argv)
+{
+  //inputan
+  cout << "angka pertama:";
+  cin >> angka1;
+  cout << "angka kedua:";
+  cin >> angka2;
+  
+  //hasilnya
+ 
+ //tambah
+ cout << "hasil tambah: ";
+ cout << angka1 + angka2;
+ 
+}
